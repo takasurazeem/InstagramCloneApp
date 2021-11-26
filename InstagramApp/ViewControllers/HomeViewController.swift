dca91580e11319a1ad03a6e08b15f3f0d4a7bab7
Takasur Azeem <takasurazeem@carecloud.com>
2021-11-26 22:24:45 +0500

    Storyboards and ViewControllers
    Signed-off-by: Takasur Azeem <takasurazeem@carecloud.com>

---
+++ InstagramApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  InstagramApp
+//
+//  Created by Takasur A. on 26/11/2021.
+//  Copyright © 2021 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ InstagramApp/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  InstagramApp
+//
+//  Created by Takasur A. on 26/11/2021.
+//  Copyright © 2021 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
