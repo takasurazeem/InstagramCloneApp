71f6d4cea7e4dd10530e8cb3e32ee8cc927c1917
Takasur Azeem <takasurazeem@carecloud.com>
2021-11-27 18:11:29 +0500

    cosmetic changes and ui connections to cell classes for feedcell
    Signed-off-by: Takasur Azeem <takasurazeem@carecloud.com>

--- InstagramApp/Views/StoriesTableViewCell/FeedTableViewCell.swift
+++ InstagramApp/Views/StoriesTableViewCell/FeedTableViewCell.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameTitleButton: UIButton!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var likesCountLabel: UILabel!
+    @IBOutlet weak var postCommentLabel: UILabel!
+    @IBOutlet weak var commentCountButton: UIButton!
+    @IBOutlet weak var dateLabel: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        selectionStyle = .none
+        
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
+        profileImage.layer.masksToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
