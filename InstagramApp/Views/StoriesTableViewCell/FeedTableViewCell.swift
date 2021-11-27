817b303ad4a9fec60b33ea8f68a8479a171a4139
Takasur Azeem <takasurazeem@carecloud.com>
2021-11-27 16:47:05 +0500

    creates feed tableview cell and add constraints
    Signed-off-by: Takasur Azeem <takasurazeem@carecloud.com>

---
+++ InstagramApp/Views/StoriesTableViewCell/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  InstagramApp
+//
+//  Created by Takasur A. on 27/11/2021.
+//  Copyright © 2021 Gwinyai Nyatsoka. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
